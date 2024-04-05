f341273ad99fa80bb3b6a6c1b80c3938b5db038d
Temrus <rogusepareidolia@gmail.com>
2024-04-05 15:06:20 +0300

    added HTML templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,50 +2,71 @@ package main
 
 import (
 	"fmt"
+	"html/template" // New import
+	"log"           // New import
 	"net/http"
 	"strconv"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"C:\\Users\\mk\\snippetbox\\ui\\html\\home.page.tmpl",
+		"C:\\Users\\mk\\snippetbox\\ui\\html\\base.layout.tmpl",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file p
+	// as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	// ts, err := template.ParseFiles("C:\\Users\\mk\\snippetbox\\ui\\html\\home.page.tmpl") //different path formatting on windows
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	http.Error(w, "Internal Server Error", 500)
+	// 	return
+	// }
+
+	// We then use the Execute() method on the template set to write the templa
+	// content as the response body. The last parameter to Execute() represents
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-
 }
-
-// Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check whether the request is using POST or not.
-	// If it's not, use the w.WriteHeader() method to send a 405 status code and
-	// the w.Write() method to write a "Method Not Allowed" response body. We
-	// then return from the function so that the subsequent code is not executed
-
-	//disallows access to create snippet if not POST
 	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST") //Another improvement we can make is to include an Allow: POST header
-		//with every 405 Method Not Allowed response to let the user know
-		//which request methods are supported for that particular URL
+		w.Header().Set("Allow", "POST")
 		http.Error(w, "Method Not Allowed", 405)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
-
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,24 +6,11 @@ import (
 )
 
 func main() {
-
-	//http.HandleFunc("/", home)
-	//http.HandleFunc("/snippet", showSnippet)
-	//http.HandleFunc("/snippet/create", createSnippet)
-
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	// if pattern matched, handler called
+	mux := http.NewServeMux() //declaring a local servemux
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-
-	// Use the http.ListenAndServe() function to start a new web server. We pas
-	// two parameters: the TCP network address to listen on (in this case ":4000
-	// and the servemux we just created. If http.ListenAndServe() returns an er
-	// we use the log.Fatal() function to log the error message and exit.
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", mux) //mil instead of mux for the default router
 	log.Fatal(err)
 }
